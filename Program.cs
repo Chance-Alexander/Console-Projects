b0bbc6fe3ae9c007233ddd97035d52a8ce2df9b3
Chance Alexander <52496309+Chance-Alexander@users.noreply.github.com>
2019-08-23 19:38:33 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyRoulette;
+using static System.Console;
+
+namespace Roulette_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run run = new Run();
+            Run.Start();
+        }
+    }
+}
